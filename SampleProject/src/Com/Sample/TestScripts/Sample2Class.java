32efc7d670deaf6ec96ef953288aeda5810482c6
Paras-Qambrani <133736860+Paras-Qambrani@users.noreply.github.com>
2023-05-17 22:11:31 +0500

    Update Sample2Class.java
     I added a statement!

--- SampleProject/src/Com/Sample/TestScripts/Sample2Class.java
+++ SampleProject/src/Com/Sample/TestScripts/Sample2Class.java
@@ -3,5 +3,6 @@ package Com.Sample.TestScripts;
 public class Sample2Class {
     public void someMethod() {
         System.out.println("Hello from the second method!");
+        System.out.println("Hello from Paras roll number 86");
     }
 }
